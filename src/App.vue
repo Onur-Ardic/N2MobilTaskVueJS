7377bb4aff5173c6b8202775f6c9e4799d582f36
Onur Ardıç <onurardc@outlook.com>
2024-09-20 22:19:25 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import Sidebar from './components/Globals/Sidebar.vue'
+</script>
+
+<template>
+  <div class="flex h-screen w-screen">
+    <Sidebar />
+    <div class="flex-grow p-5 overflow-y-auto">
+      <RouterView />
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
