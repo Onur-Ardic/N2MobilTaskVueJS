7eba78657575f17d93b6854b4a2613ecf32011d7
Onur Ardıç <onurardc@outlook.com>
2024-09-22 17:46:43 +0300

    modal style has been added

--- src/components/Pages/Detail/Posts.vue
+++ src/components/Pages/Detail/Posts.vue
@@ -2,12 +2,44 @@
   <div class="relative">
     <h3 class="text-xl font-bold">Posts</h3>
     <div v-if="isLoading">Loading...</div>
-    <ul v-else>
+    <ul class="relative" v-else>
       <li v-if="localPosts.length === 0" class="text-gray-500">No Posts Available</li>
-      <li v-else v-for="post in localPosts" :key="post.id" class="mt-4">
-        <h4 class="font-bold">{{ post.title }}</h4>
-        <p>{{ post.body }}</p>
-        <button @click="openModal(post)" class="mt-2 text-blue-500">See More</button>
+      <li v-else v-for="post in localPosts" :key="post.id" class="mt-10 relative">
+        <div class="content w-[600px]">
+          <h4 class="font-medium text-[#26303E]">{{ post.title }}</h4>
+          <p class="mt-3 text-slate-500">{{ post.body }}</p>
+        </div>
+        <div @click="openModal(post)" class="see-more-btn absolute right-3 bottom-2 flex gap-3">
+          <button class="font-semibold">See More</button>
+          <svg
+            width="32"
+            height="32"
+            viewBox="0 0 32 32"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M16 20L20 16M20 16L16 12M20 16H12"
+              stroke="#4F359B"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+            <path
+              d="M16 7C23.2 7 25 8.8 25 16C25 23.2 23.2 25 16 25C8.8 25 7 23.2 7 16C7 8.8 8.8 7 16 7Z"
+              stroke="#4F359B"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+            <path
+              d="M16 10.6667L21.3333 16L16 21.3333L15.05 20.4L18.7833 16.6667H10.6666V15.3333H18.7833L15.05 11.6L16 10.6667Z"
+              fill="#4F359B"
+            />
+          </svg>
+        </div>
+
+        <div class="line w-full absolute mt-3 h-[0.5px] bg-[#D8D9DD]"></div>
       </li>
     </ul>
 
@@ -43,7 +75,6 @@ export default {
     const openModal = (post) => {
       selectedPost.value = post
       isModalOpen.value = true
-      console.log('Modal open:', isModalOpen.value)
     }
     onMounted(async () => {
       await getPosts.fetchPosts()
--- src/components/Pages/Detail/Uİ/Modal.vue
+++ src/components/Pages/Detail/Uİ/Modal.vue
@@ -1,19 +1,49 @@
 <template>
   <div class="fixed inset-0 bg-gray-800 bg-opacity-75 flex items-center justify-center">
-    <div class="bg-white rounded-lg p-5 w-11/12 md:w-3/4 lg:w-1/2 relative">
-      <button @click="$emit('close')" class="absolute top-2 right-2">Close Modal</button>
-      <h4 class="text-xl font-bold">{{ post.title }}</h4>
-      <p class="mt-2">{{ post.body }}</p>
-      <div class="mt-4">
-        <h5 class="font-bold">Yorumlar</h5>
-        <ul>
-          <li v-if="comments.length === 0" class="text-gray-500">No Comments</li>
-          <li v-else v-for="comment in comments" :key="comment.id" class="mt-2">
-            <p>
-              <strong>{{ comment.name }}:</strong> {{ comment.body }}
-            </p>
-          </li>
-        </ul>
+    <div class="bg-white rounded-2xl p-10 w-11/12 md:w-3/4 lg:w-1/2 relative">
+      <button @click="$emit('close')" class="absolute top-2 right-2">
+        <svg
+          width="20"
+          height="20"
+          viewBox="0 0 20 20"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M8 8L12 12M12 8L8 12M10 1C17.2 1 19 2.8 19 10C19 17.2 17.2 19 10 19C2.8 19 1 17.2 1 10C1 2.8 2.8 1 10 1Z"
+            stroke="#26303E"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          />
+        </svg>
+      </button>
+
+      <div class="grid grid-cols-1 md:grid-cols-2 gap-4 h-96">
+        <div class="overflow-y-auto pr-4 border-r border-gray-300">
+          <h4 class="text-xl font-bold">{{ post.title }}</h4>
+          <p class="mt-2">{{ post.body }}</p>
+        </div>
+
+        <div class="overflow-y-auto pl-4">
+          <h5 class="font-bold">Yorumlar</h5>
+          <ul class="mt-2 space-y-4">
+            <li v-if="comments.length === 0" class="text-gray-500">No Comments</li>
+            <li
+              v-else
+              v-for="comment in comments"
+              :key="comment.id"
+              class="flex items-start mt-3 space-x-3"
+            >
+              <img src="/userimage.jpg" alt="avatar" class="w-10 h-10 rounded-full object-cover" />
+
+              <div>
+                <p class="text-base">{{ comment.email }}</p>
+                <p class="text-gray-600 text-xs mt-2">{{ comment.body }}</p>
+              </div>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   </div>
@@ -43,3 +73,32 @@ export default {
   },
 }
 </script>
+
+<style scoped>
+.overflow-y-auto {
+  max-height: 24rem;
+  overflow-y: auto;
+  scrollbar-width: thin;
+  scrollbar-color: #d8d9dd #f0f0f0;
+}
+
+.overflow-y-auto::-webkit-scrollbar {
+  width: 8px;
+  border-radius: 30px;
+}
+
+.overflow-y-auto::-webkit-scrollbar-track {
+  background: #f0f0f0;
+  border-radius: 30px;
+}
+
+.overflow-y-auto::-webkit-scrollbar-thumb {
+  background-color: #d8d9dd;
+  border-radius: 10px;
+  border: 2px solid #f0f0f0;
+}
+
+.space-y-4 > :not(:first-child) {
+  margin-top: 1rem;
+}
+</style>
